e416a456acbdb5ad093e7fbd904e690fd86df1cd
Omar Lopez <olopez1791@richmondhillhs.org>
2020-12-02 08:11:04 +0000

    My-First-React-Project

--- src/App.js
+++ src/App.js
@@ -5,20 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Omar Lopez</h1>
+      <h2> All About Me</h2>
+      <p>I go by many names Omar, Omez, and Peetie Peetie (Dont Ask)</p>
+      <p>My favorite quote is from Friedrich Nietzsche who said,"God is dead. God remains dead. And we have killed him. How shall we comfort ourselves, the murderers of all murderers."</p>
+      <p>My hobbies are listening to music, working out, playing football and basketball, playing guitar, playing videogames, and hanging out with friends.</p>
     </div>
   );
 }
